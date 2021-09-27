0b480a6ffdf8854efbec0d3c23209b1d23c486ef
Giovanni Bajo <rasky@develer.com>
2021-09-28 00:14:59 +0200

    audioconv64: add support for XM64.
    The conversion requires two main steps. First, unrolling the ping-pong
    loops to forward loops, and add overreads to all waveforms. Second,
    calculate the exact minimum size for each XM channel so that the player
    is then able to configure the minimum amount of memory in the mixer.

---
+++ include/xm64internal.h
@@ -0,0 +1,9 @@
+#ifndef __LIBDRAGON_XM64_INTERNAL_H
+#define __LIBDRAGON_XM64_INTERNAL_H
+
+// This header file is not installed, and we want to keep the original libxm
+// header files separate for vendoring purposes. So just include them here.
+#include "../src/audio/libxm/xm.h"
+#include "../src/audio/libxm/xm_internal.h"
+
+#endif
