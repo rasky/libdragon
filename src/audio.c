8f73aa675ab1eb2b5fe9a985a1ba0970cfca26d9
Giovanni Bajo <rasky@develer.com>
2021-10-31 23:01:14 +0100

    audio: implement support for reset process
    The audio library is changed to stop sending audio buffer to AI if a
    reset is imminent.
    
    The mixer library is changed to do a global volume fadeout during the
    reset process.

--- src/audio.c
+++ src/audio.c
@@ -138,6 +138,14 @@ static void audio_callback()
         return;
     }
 
+    /* If we are more than 150ms into the reset process, stop
+       playing audio altogether, to avoid scheduling a new chunk
+       too close to the actual reset instant. */
+    if(exception_reset_time() > TICKS_FROM_MS(150))  
+    {
+        return;
+    }
+
     /* Disable interrupts so we don't get a race condition with writes */
     disable_interrupts();
 
