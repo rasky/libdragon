886ef7fb54c86280d0db2cf307938bc758edeb20
Giovanni Bajo <rasky@develer.com>
2021-11-06 21:24:21 +0100

    [FASTMATH] remove sqrtf and add atan2f

--- src/fmath.c
+++ src/fmath.c
@@ -7,11 +7,14 @@
 #include <string.h>
 #include <stdint.h>
 
+static const float pi_hi            = 3.14159274e+00f; // 0x1.921fb6p+01
+static const float pi_lo            =-8.74227766e-08f; // -0x1.777a5cp-24
+static const float half_pi_hi       =  1.57079637e+0f; //  0x1.921fb6p+0
+// static const float half_pi_lo       = -4.37113883e-8f; // -0x1.777a5cp-25
+
 float fm_sinf(float x) {
     // Approximation of sine to 5 ULP with Chebyshev polynomials
     // http://mooooo.ooo/chebyshev-sine-approximation/
-    const float pi_hi            = 3.14159274e+00f; // 0x1.921fb6p+01
-    const float pi_lo            =-8.74227766e-08f; // -0x1.777a5cp-24
     float p, s;
 
     // This function has been designed to operate in the [-π, +π] range, so
@@ -30,34 +33,22 @@ float fm_sinf(float x) {
 }
 
 float fm_cosf(float x) {
-    const float half_pi = 1.57079637e+0f; //  0x1.921fb6p+0
-    return fm_sinf(half_pi - x);
+    return fm_sinf(half_pi_hi - x);
 }
 
-/** @brief Calculate `z / sqrt(x)`. 
- * 
- * This primitive can be used for both `1/sqrt(x)` (z=1), and `sqrt(x)` (z=x).
- *
- * We use the algorithm presented in this 2018 paper:
- * Improving the Accuracy of the Fast Inverse Square Root by Modifying
- * Newton–Raphson Corrections (Walczyk, Moroz, Cieslinski).
- * 
- * It computes an initial estimation (similar to the famous Quake's version)
- * and two subsequent debiased Newton-Raphson iterations. This version does
- * not handle denormals as they are not supported by MIPS VR4300 FPU anyway.
- * 
- * The average numerical error is ~= 0.35 * 10e-6.
- **/
-__attribute__((noinline))
-static float __inv_sqrtf(float x, float z) {
-    int32_t i = BITCAST_F2I(x);
-    i = 0x5F200000 - (i>>1);
-    float y = BITCAST_I2F(i);
-    y *= 1.68191391f - 0.703952009f * x * y * y;
-    y *= 1.50000036f - 0.500000053f * x * y * y;
-    return y * z;
+float fm_atan2f(float y, float x) {
+    // Approximation of atan2f using a polynomial minmax approximation in [0,1]
+    // calculated via the Remez algorithm (https://math.stackexchange.com/a/1105038).
+    // The reported error is 6.14e-4, so it's precise for at least three decimal
+    // digits which is usually more than enough for angles.
+    float ay = fabsf(y);
+    float ax = fabsf(x);
+    float a = (ay < ax) ? ay/ax : ax/ay;
+    float s = a * a;
+    float r = ((-0.0464964749f * s + 0.15931422f) * s - 0.327622764f) * s * a + a;
+    if (ay > ax)
+        r = half_pi_hi - r;
+    if (BITCAST_F2I(x) < 0) r = pi_hi - r;
+    return copysignf(r, y);
 }
-
-float fm_inv_sqrtf(float x) { return __inv_sqrtf(x, 1.0f); }
-float fm_sqrtf(float x)     { return __inv_sqrtf(x, x);    }
 
